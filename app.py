68b138f1584003bc5280dbe84d28584804d1a526
^_^ <32826270+hcm444@users.noreply.github.com>
2024-01-14 13:55:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-
+import base64
 from datetime import datetime, timedelta
 import re
+from io import BytesIO
+
 from flask_caching import Cache
 from PIL import Image, ImageDraw, ImageFont
 import threading
@@ -8,7 +10,7 @@ import colorsys
 import time
 import csv
 import logging
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, send_file
 import random
 import string
 from difflib import SequenceMatcher
@@ -37,11 +39,45 @@ fingerprint_post_counts = {}
 
 logging.basicConfig(level=logging.DEBUG)
 
-def generate_captcha():
+
+def generate_captcha_image():
     captcha_length = 6
     captcha_chars = string.ascii_uppercase + string.digits
-    captcha = ''.join(random.choice(captcha_chars) for _ in range(captcha_length))
-    return captcha
+    captcha_code = ''.join(random.choice(captcha_chars) for _ in range(captcha_length))
+
+    # Create a larger image with the captcha code
+    original_width, original_height = 150, 100
+    zoom_factor = 0.3  # Increase this value to zoom in further
+    zoomed_width, zoomed_height = int(original_width * zoom_factor), int(original_height * zoom_factor)
+
+    image = Image.new('RGB', (zoomed_width, zoomed_height), color=(255, 255, 255))
+    draw = ImageDraw.Draw(image)
+
+    # Use the default font
+    font = ImageFont.load_default()
+
+    # Get the size of the text to be drawn
+    text_width, text_height = draw.textsize(captcha_code, font=font)
+
+    # Center the text in the larger image
+    text_position = ((zoomed_width - text_width) // 2, (zoomed_height - text_height) // 2)
+
+    draw.text(text_position, captcha_code, font=font, fill=(0, 0, 0))
+
+    # Resize the image to the original dimensions
+    image = image.resize((original_width, original_height), Image.ANTIALIAS)
+
+    # Save the image to a BytesIO object
+    image_io = BytesIO()
+    image.save(image_io, 'PNG')
+    image_io.seek(0)
+
+    # Convert the image data to base64 encoding
+    base64_image = base64.b64encode(image_io.getvalue()).decode('utf-8')
+
+    return captcha_code, base64_image
+
+
 def generate_device_fingerprint():
     user_agent = request.headers.get('User-Agent', '')
     ip_address = request.remote_addr
@@ -134,10 +170,11 @@ def snake():
     return render_template('snake.html')
 
 
+# In the home route
 @app.route('/')
 def home():
-    captcha = generate_captcha()
-    session['captcha'] = captcha
+    captcha_code, captcha_image = generate_captcha_image()
+    session['captcha'] = captcha_code
     page = int(request.args.get('page', 1))
     messages_per_page = POSTS_PER_PAGE
     total_pages = (len(message_board) + messages_per_page - 1) // messages_per_page
@@ -148,7 +185,8 @@ def home():
 
     messages_to_display = reversed_message_board[start_index:end_index]
 
-    return render_template('index.html', messages=messages_to_display, total_pages=total_pages, current_page=page,captcha=captcha)
+    # Pass both messages and captcha information to the template
+    return render_template('index.html', messages=messages_to_display, total_pages=total_pages, current_page=page, captcha_image=captcha_image)
 
 
 @app.route('/post', methods=['POST'])
