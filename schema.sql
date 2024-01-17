895eb312394c84f658a4e3e750ccea1f790f1220
^_^ <32826270+hcm444@users.noreply.github.com>
2024-01-17 13:12:21 -0800

    Delete schema.sql

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS posts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    post_number INTEGER,
-    timestamp DATETIME,
-    message TEXT,
-    parent_post_number INTEGER
-);
